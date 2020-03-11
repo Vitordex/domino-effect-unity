dc7672edff9d1287856e45fd94c517b997c0ea2f
Vitor Ribeiro Pais Del Pezzo <vitordexjesus@gmail.com>
2020-03-10 23:47:00 -0300

    feat(game): Add a spawn limit to the dominoe's

---
+++ Assets/Runtime/Spawn/SpawnUIBinder.cs
@@ -0,0 +1,26 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace domino_effect.Runtime.Spawn {
+  public class SpawnUIBinder : MonoBehaviour {
+    public TextMeshProUGUI _uiText;
+    private SpawnManager _manager;
+
+    private void Awake() {
+      _manager = FindObjectOfType<SpawnManager>();
+      _manager.OnSpawn.AddListener(UpdateText);
+
+      UpdateText(0);
+    }
+
+    public void UpdateText(int count) {
+      var padCount = ConvertToTwoDigitString(count);
+      var padLimit = ConvertToTwoDigitString(_manager.SpawnLimit);
+      _uiText.text = $"{padCount}|{padLimit}";
+    }
+
+    private string ConvertToTwoDigitString(int number) {
+      return number.ToString().PadLeft(2, '0');
+    }
+  }
+}

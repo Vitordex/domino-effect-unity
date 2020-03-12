88229a6ac5de6f2d548843df48922f86506bf844
Vitor Ribeiro Pais Del Pezzo <vitordexjesus@gmail.com>
2020-03-11 23:47:12 -0300

    feat(game): Add physics simulation controls to game
    Issues: #7

---
+++ Assets/PhysicsSimulation/SimulationController.cs
@@ -0,0 +1,66 @@
+﻿using domino_effect.Runtime.Spawn;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace domino_effect.PhysicsSimulation
+{
+    public class SimulationController : MonoBehaviour
+    {
+        private List<(Vector3, Quaternion)> _simulationStartMap = new List<(Vector3, Quaternion)>();
+
+        public UnityEvent OnStart;
+        public UnityEvent OnStop;
+        public UnityEvent OnClear;
+
+        private TimeController _timeController;
+        private SpawnManager _spawnManager;
+
+        private void Awake()
+        {
+            _spawnManager = FindObjectOfType<SpawnManager>();
+
+            _timeController = GetComponentInChildren<TimeController>();
+            _timeController.StopTime();
+        }
+
+        public void StartSimulation()
+        {
+            _timeController.StartTime();
+
+            var rawSpawns = _spawnManager.Spawns;
+            _simulationStartMap = rawSpawns.Select((spawn) => (spawn.transform.position, spawn.transform.rotation)).ToList();
+
+            OnStart.Invoke();
+        }
+
+        public void StopSimulation()
+        {
+            _timeController.StopTime();
+
+            var spawns = _spawnManager.Spawns;
+            for (int i = 0; i < spawns.Count; i++)
+            {
+                var spawn = spawns[i];
+                var initialTransform = _simulationStartMap[i];
+                spawn.transform.position = initialTransform.Item1;
+                spawn.transform.rotation = initialTransform.Item2;
+            }
+
+            _simulationStartMap.Clear();
+
+            OnStop.Invoke();
+        }
+
+        public void ClearSimulation()
+        {
+            _timeController.StopTime();
+            _spawnManager.ClearSpawns();
+            _simulationStartMap.Clear();
+
+            OnClear.Invoke();
+        }
+    }
+}
--- Assets/Runtime/Spawn/SpawnManager.cs
+++ Assets/Runtime/Spawn/SpawnManager.cs
@@ -2,37 +2,57 @@
 using UnityEngine;
 using UnityEngine.Events;
 
-namespace domino_effect.Runtime.Spawn {
-  public class SpawnManager : BaseMonoBehaviour {
-    public GameObject Spawn;
-    public int SpawnLimit;
-    public Transform SpawnPoint;
+namespace domino_effect.Runtime.Spawn
+{
+    public class SpawnManager : BaseMonoBehaviour
+    {
+        public GameObject Spawn;
+        public int SpawnLimit;
+        public Transform SpawnPoint;
 
-    private List<GameObject> _spawns = new List<GameObject>();
+        private List<GameObject> _spawns = new List<GameObject>();
 
-    public IntEvent OnSpawn = new IntEvent();
+        public List<GameObject> Spawns {
+            get => _spawns;
+        }
 
-    private void Awake() {
-      if (Spawn == null) Debug.LogError($"[{name}][Spawner][Awake] Please define an object prefab to Spawn on click");
-      if (SpawnPoint == null) SpawnPoint = Transform;
+        public IntEvent OnSpawn = new IntEvent();
+
+        private void Awake()
+        {
+            if (Spawn == null) Debug.LogError($"[{name}][Spawner][Awake] Please define an object prefab to Spawn on click");
+            if (SpawnPoint == null) SpawnPoint = Transform;
+        }
+
+        private void Update()
+        {
+            if (Spawn == null) return;
+
+            var mouseDown = Input.GetMouseButtonDown(0);
+            if (_spawns.Count < SpawnLimit && mouseDown)
+            {
+                var spawned = SpawnObject();
+                _spawns.Add(spawned);
+                OnSpawn.Invoke(_spawns.Count);
+            }
+        }
+
+        private GameObject SpawnObject()
+        {
+            return Instantiate(Spawn, SpawnPoint.position, Quaternion.identity);
+        }
+
+        public void ClearSpawns()
+        {
+            foreach (var spawn in _spawns)
+            {
+                Destroy(spawn);
+            }
+
+            _spawns.Clear();
+        }
     }
 
-    private void Update() {
-      if (Spawn == null) return;
-
-      var mouseDown = Input.GetMouseButtonDown(0);
-      if (_spawns.Count < SpawnLimit && mouseDown) {
-        var spawned = SpawnObject();
-        _spawns.Add(spawned);
-        OnSpawn.Invoke(_spawns.Count);
-      }
-    }
-
-    private GameObject SpawnObject() {
-      return GameObject.Instantiate(Spawn, SpawnPoint.position, Quaternion.identity);
-    }
-  }
-
-  [SerializeField]
-  public class IntEvent : UnityEvent<int> { }
+    [SerializeField]
+    public class IntEvent : UnityEvent<int> { }
 }

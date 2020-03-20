da0065f8036532129ec77322b43a842282cf1a84
Vitor Ribeiro Pais Del Pezzo <vitordexjesus@gmail.com>
2020-03-19 23:21:05 -0300

    feat(game): Add level selection ui
    Issues: #17

---
+++ Assets/LevelButton/LevelButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace domino_effect.LevelButtons
+{
+    [RequireComponent(typeof(Button))]
+    public class LevelButton : MonoBehaviour
+    {
+        private Button _button;
+        [SerializeField] private TextMeshProUGUI _textBox = null;
+
+        private void Awake() {
+            _button = GetComponent<Button>();
+        }
+        
+        public void SetOnClick(UnityAction call) {
+            _button.onClick.AddListener(call);
+        }
+
+        public void SetText(string text) {
+            _textBox.text = text;
+        }
+    }
+}
---
+++ Assets/LevelButton/LevelSelectManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace domino_effect.LevelButtons
+{
+    public class LevelSelectManager : MonoBehaviour
+    {
+        public LevelButton ButtonPrefab;
+        public Transform SelectionHolder;
+
+        private void Awake() {
+            var scenesCount = SceneManager.sceneCountInBuildSettings;
+            var levelCount = 0;
+            for (int i = 0; i < scenesCount; i++)
+            {
+                var scene = SceneUtility.GetScenePathByBuildIndex(i);
+                if(!Regex.IsMatch(scene, "Level_\\d{2}")) continue;
+
+                var buttonObject = Instantiate(ButtonPrefab, SelectionHolder);
+                var button = buttonObject.GetComponent<LevelButton>();
+                button.SetOnClick(() => SceneManager.LoadSceneAsync(scene));
+                button.SetText($"{++levelCount:00}");
+            }
+        }
+    }
+}
